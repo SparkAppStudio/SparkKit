328a74e98a49bb1703131cad28c732b8c81bee54
John Regner <regnerjr@gmail.com>
2017-10-19 20:37:23 -0700

    Initial Add of the Framework.

---
+++ SparkKit/SparkKit.h
@@ -0,0 +1,19 @@
+//
+//  SparkKit.h
+//  SparkKit
+//
+//  Created by John Regner on 10/19/17.
+//  Copyright © 2017 Spark App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SparkKit.
+FOUNDATION_EXPORT double SparkKitVersionNumber;
+
+//! Project version string for SparkKit.
+FOUNDATION_EXPORT const unsigned char SparkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SparkKit/PublicHeader.h>
+
+
