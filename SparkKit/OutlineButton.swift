f07f84317899fc5faf67322c39b1c8775bd7a438
John Regner <regnerjr@gmail.com>
2017-11-11 15:10:41 -0800

    Set some doc comments

--- SparkKit/OutlineButton.swift
+++ SparkKit/OutlineButton.swift
@@ -4,7 +4,14 @@
 
 import UIKit
 
+
+/// A Button with a rounded rect outline around it.
+/// The button `title` can be set to a custom string
+/// The title and outline will share the same `color: UIColor`
 public class OutlineButton: UIButton {
+
+    /// Set's the color for the title and the outline
+    /// Default is white
     public var color: UIColor = .white {
         didSet {
             setNeedsDisplay()
@@ -12,11 +19,13 @@ public class OutlineButton: UIButton {
         }
     }
 
+    /// The title text to be set, used in the center of the button
     public var title: String {
         set { label.text = newValue }
         get { return label.text ?? "" }
     }
 
+    /// Strokes the outline of the button
     public override func draw(_ rect: CGRect) {
         let insetRect = rect.insetBy(dx: 4, dy: 4)
         let outlinePath = UIBezierPath(roundedRect: insetRect, cornerRadius: 12.0)
@@ -48,6 +57,8 @@ public class OutlineButton: UIButton {
         addSubview(label)
     }
 
+    /// Animated the button touch down
+    /// Color is slightly dimmed, and button size shrinks
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         self.label.textColor = color.withAlphaComponent(0.6)
@@ -58,6 +69,8 @@ public class OutlineButton: UIButton {
         }
     }
 
+    /// Animates button on touch up
+    /// Color is set back to normal and size is set back to identity
     public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         self.label.textColor = color.withAlphaComponent(1.0)
